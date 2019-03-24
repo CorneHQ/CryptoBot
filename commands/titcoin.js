697268d4e9797cfaa266b07c8952142e08f63cf0
CorneHQ <cornedenbreejen@caiway.net>
2019-03-24 18:07:49 +0100

    [TASK] Added currency

--- commands/titcoin.js
+++ commands/titcoin.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     axios.get('https://min-api.cryptocompare.com/data/price', {
         params: {
 		  fsym: "TIT",
-		  tsyms: "USD,EUR",
+		  tsyms: "USD,EUR,CNY,RUB",
 		  api_key: "897761cbb66df68597ccd12a08ba51695a35e4ba5f858843c3e0a2ab130a3adf"
         }
     })
@@ -14,8 +14,10 @@ module.exports.run = async (bot, message, args) => {
 		let titcoinEmbed = new Discord.RichEmbed()
 		.setDescription("Auroracoin Values")
 		.setColor("#50C7C7")
-		.addField(":euro: Euro", response.data.EUR)
-		.addField(":dollar: Dollar", response.data.USD)
+		.addField("Euro", response.data.EUR)
+		.addField("American Dollar", response.data.USD)
+		.addField("Yuan", response.data.CNY)
+		.addField("Rouble", response.data.RUB)
 		.setFooter("CryptoBot By CorneHQ");
 		
 		message.channel.send(titcoinEmbed);
