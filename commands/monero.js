8c74b963a3c5ef0b7d7ed0ddad1bbc89ff13554e
CorneHQ <cornedenbreejen@caiway.net>
2019-03-24 16:16:19 +0100

    [FEATURE] Added crypto's

---
+++ commands/monero.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const axios = require("axios");
+const chalk = require("chalk");
+
+module.exports.run = async (bot, message, args) => {
+    axios.get('https://min-api.cryptocompare.com/data/price', {
+        params: {
+		  fsym: "XMR",
+		  tsyms: "USD,EUR",
+		  api_key: "897761cbb66df68597ccd12a08ba51695a35e4ba5f858843c3e0a2ab130a3adf"
+        }
+    })
+    .then(function (response) {
+		let moneroEmbed = new Discord.RichEmbed()
+		.setDescription("Monero Values")
+		.setColor("#50C7C7")
+		.addField(":euro: Euro", response.data.EUR)
+		.addField(":dollar: Dollar", response.data.USD)
+		.setFooter("CryptoBot By CorneHQ");
+		
+		message.channel.send(moneroEmbed);
+
+		return;
+    })
+    .catch(function (error) {
+		console.log(chalk.red(error));
+		
+		return;
+    })
+}
+
+module.exports.help = {
+    name: "monero"
+}
